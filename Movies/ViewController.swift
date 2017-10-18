949aeb16e45ce12ba59937a8fdb1832dd2349942
Kacper Kowalski <kowal@Mac-mini-Kacper.local>
2017-10-18 11:50:35 +0200

    created custom MovieCell class

--- Movies/ViewController.swift
+++ Movies/ViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        view.backgroundColor = .white
+        self.title = "MOVIE LIST"
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //
+        return 1
+    }
+    
+    
+    
+    
+    
+    
+    
 
 }
 
